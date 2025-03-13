e3411e9de0c1721c4d2f73b403927234fa41aceb
simone2204 <48283393+simone2204@users.noreply.github.com>
2025-03-13 16:42:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-/*Date Function*/
 function updateDate() {
     const today = new Date();
   
-    // Format the date to the desired string format
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const options = { 
+                      weekday: 'long', 
+                      year: 'numeric', 
+                      month: 'long', 
+                      day: 'numeric' 
+                    };
     const formattedDate = today.toLocaleDateString('en-US', options);
   
-    // Select the element with ID "Date" and update its content
     const dateElement = document.querySelector('#Date');
     if (dateElement) {
       dateElement.textContent = formattedDate;
@@ -14,15 +16,12 @@ function updateDate() {
       console.error('Element with ID "Date" not found.');
     }
   }
-  
-  updateDate(); // Call the function to display the date when the page loads
+updateDate();
 
-//---------------------------------------------------------------------------
-// 1) UTILIZZARE ADDEVENTLISTENER()
 
 const button = document.getElementById("readMore");
-button.addEventListener("click", (event) =>{
-  if(paragraphs.style.visibility === "visible"){
+button.addEventListener("click", () =>{
+  if(document.getElementById("paragraphs").style.visibility == "visible"){
     document.getElementById("paragraphs").style.visibility = "hidden";
     button.textContent = "read more";
     }
@@ -32,13 +31,12 @@ button.addEventListener("click", (event) =>{
     }
 });
 
-//------------------------------------------------------------------------------
-// 2) USARE CREATEELEMENT()
+
 const newContainer = document.createElement("div");
 newContainer.classList.add("NewContainer");
 newContainer.id = "newContainer";
-const box3 = document.getElementById("GamesContainer");
-box3.parentNode.insertBefore(newContainer, box3);
+const gamesContainer = document.getElementById("GamesContainer");
+gamesContainer.parentNode.insertBefore(newContainer, gamesContainer);
 
 const newElement = document.createElement("h1");
 newElement.id = "newH1"
@@ -47,22 +45,6 @@ newElement.textContent = "Click me to change the font-style";
 document.getElementById("newContainer").append(newElement);
 
 
-//-------------------------------------------------------------------------
-// 3) MODIFICARE L'URL DI UN'IMMAGINE TRAMITE SRC
-const image = document.getElementById("images");
-const button_1 = document.getElementById("button");
-const images = ["sport/sport.webp", "sport/conor.webp", "sport/tennis.jpg"];
-let index = 0;
-function switchImages() {
-  index++;
-  if (index >= images.length) index = 0;
-  image.src = images[index];
-}
-setInterval(switchImages, 10000);
-button_1.addEventListener("click", switchImages);
-
-//-----------------------------------------------------------------------
-// 4) UTILIZZARE CLASSLIST
 const button_2 = document.getElementById("newH1");
 const button_3 = document.getElementById("title");
 const fonts = ["Courier New","Cambria","Franklin Gothic Medium","Segoe UI","Georgia","Impact"];
@@ -76,32 +58,43 @@ function changeFont() {
 }
 button_2.addEventListener("click", changeFont);
 
-//----------------------------------------------------------------------
-button_3.addEventListener("mouseover", event => {
+
+const image = document.getElementById("images");
+const button_1 = document.getElementById("button");
+const images = ["sport/sport.webp", "sport/conor.webp", "sport/tennis.jpg"];
+let index = 0;
+function switchImages() {
+  index++;
+  if (index >= images.length) index = 0;
+  image.src = images[index];
+}
+setInterval(switchImages, 10000);
+button_1.addEventListener("click", switchImages);
+
+
+button_3.addEventListener("mouseover", () => {
   button_3.classList.add("hover");
 });
-button_3.addEventListener("mouseout", event => {
+button_3.addEventListener("mouseout", () => {
   button_3.classList.remove("hover");
 });
 
-//------------------------------------------------------------------------
-// ATTRIBUTI DATA-*
+
 const images_1 = document.querySelectorAll("img");
 images_1.forEach(image => {
-  image.addEventListener("mouseover", event =>{
+  image.addEventListener("mouseover", () => {
     const dataAttr = image.getAttribute("data-category");
     console.log(`${dataAttr}`);
     image.style.opacity='0.8';
   });
-  image.addEventListener("mouseout", event =>{
+  image.addEventListener("mouseout", () => {
     image.style.opacity='1.0';
   });
 });
 
-//------------------------------------------------------------------------
-// MODAL-VIEW
+
 const image_2 = document.querySelector("#Container-3 img");
-image_2.addEventListener("click", event => {
+image_2.addEventListener("click", () => {
   const modal = document.createElement("div");
   modal.classList.add("modal-view");
 
@@ -111,18 +104,17 @@ image_2.addEventListener("click", event => {
   
   modal.appendChild(modalImage);
   document.body.appendChild(modal);
-  modal.addEventListener("click", event => {
+  modal.addEventListener("click", () => {
     document.body.removeChild(modal);
   });
 });
 
-//----------------------------------------------------------------------------
-// NYT API TO RETRIEVE NEWS AND ARTICLES WITH PHP
+
 document.addEventListener("DOMContentLoaded", () => {
-  firstArticle();
+  loadArticles();
 });
 
-function firstArticle() {
+function loadArticles() {
   const h1 = document.getElementById("header-1");
   const p = document.getElementById("paragrafo");
   const sp = document.getElementById("sottoParagrafo");
@@ -136,106 +128,95 @@ function firstArticle() {
   h1.textContent = "Loading....";
   p.textContent = "Loading...";
   sp.textContent = "loading...";
+  image.textContent = "Loading image...";
 
-  fetch('http://127.0.0.1/Projects/News.php')
+  fetch('http://127.0.0.1/Projects/News_.php')
   .then(response => response.json())
   .then(data => {
-    console.log(data);
-    if(data.title && data.abstract && data.lead_paragraph) {
+    console.log("First fetch data:", data);
+
+    if (data.title && data.abstract && data.lead_paragraph) {
       h1.textContent = data.title;
       p.textContent = data.abstract;
       sp.textContent = data.lead_paragraph;
 
       const articleId = 1;
-      
+
       p.innerHTML = `${data.abstract} 
         <span class="like-icon" data-article="${articleId}">👍</span>
-        <span class="like-count" id="like-article${articleId}">0</span>`;
+        <span class="like-count" id="like-article${articleId}">0</span>
+        <button class="comment" data-article="${articleId}">Share a comment</button>`;
 
       if (data.img) {
-      const img = document.createElement("img");
-      img.src = data.img;
-      img.alt = "article image";
-      img.classList.add("img-api");
-      image.innerHTML = "";
-      image.appendChild(img);
-     } 
-     else image.textContent = "No image available";
-
-    }
-      else {
-        h1.textContent = "errore nel caricamento...";
-        p.textContent = "errore nel caricamento...";
-        sp.textContent = "errore nel caricamento...";
+        const img = document.createElement("img");
+        img.src = data.img;
+        img.alt = "Article image";
+        img.classList.add("img-api");
+        image.innerHTML = "";
+        image.appendChild(img);
+      } else {
+        image.textContent = "No image available";
       }
-      h1.classList.add('visible');
-      p.classList.add('visible');
+    } else {
+      h1.textContent = "Error loading article";
+      p.textContent = "Error loading article";
+      sp.textContent = "Error loading article";
+    }
   })
   .catch(error => {
     console.error("Error fetching data:", error);
-    h1.textContent = "Errore nella richiesta";
-    p.textContent = "Errore nella richiesta";
-    sp.textContent = "Errore nella richiesta";
-  })
-  .finally(() => {
-    h1.classList.add('visible');
-    p.classList.add('visible');
+    h1.textContent = "Request error...";
+    p.textContent = "Request error...";
+    sp.textContent = "Request error...";
   });
+
+  fetch('http://127.0.0.1/Projects/News.php')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    if (Array.isArray(data) && data.length > 0) {
+      // Pulisci il contenitore articoli prima di aggiungere nuovi articoli
+      document.getElementById("articles-container").innerHTML = "";
+
+      // Itera attraverso gli articoli e crea gli elementi HTML per ciascuno
+      data.forEach((article, index) => {
+        const articleDiv = document.createElement("div");
+        articleDiv.classList.add("article");
+
+        articleDiv.innerHTML = `
+          <h1>${article.title}</h1>
+          <p>${article.abstract}</p>
+          <p>${article.lead_paragraph}</p>
+          ${article.img ? `<img src="${article.img}" alt="Article image">` : "<p>No image available</p>"}
+          <span class="like-icon" data-article="${index + 1}">👍</span>
+          <span class="like-count" id="like-article${index + 1}">0</span>
+          <button class="comment" data-article="${index + 1}">Share a comment</button>`;
+
+        // Aggiungi l'articolo al contenitore
+        document.getElementById("articles-container").appendChild(articleDiv);
+      });
+    }
+      else {
+        h1.textContent = "loading error...";
+        p.textContent = "loading error...";
+        sp.textContent = "loading error...";
+      }
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+    h1.textContent = "Request error...";
+    p.textContent = "Request error...";
+    sp.textContent = "Request error...";
+  })
 }
 
-//----------------------------------------------------------------
-// LOGIN - LOGOUT MANAGER
-document.addEventListener("DOMContentLoaded", () => {
-  function getCookie(name) {
-      let cookies = document.cookie.split("; ");
-      for (let i = 0; i < cookies.length; i++) {
-          let cookie = cookies[i].split("=");
-          if (cookie[0] === name) {
-              return decodeURIComponent(cookie[1]);
-          }
-      }
-      return null;
-  }
 
-  let userEmail = getCookie("user_email");
-  let loginLink = document.getElementById("login");
-  let userInfo = document.getElementById("user-info");
-
-  if (userEmail) {
-      
-      loginLink.style.display = "none"; // Nasconde il link di login
-      userInfo.innerHTML = `<span>${userEmail}</span>
-      <button id="Logout">Logout</button>`;
-
-      
-      document.getElementById("Logout").addEventListener("click", () => {
-          document.cookie = "user_email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-          location.reload();
-      });
-  }
-});
-
-//----------------------------------------------------------------------------
-// LIKES BUTTONS
-document.addEventListener("click", function (event) {
+document.addEventListener("click", (event) => {
   if (event.target.classList.contains("like-icon")) {
-    
-    function getCookie(name) {
-      let cookies = document.cookie.split("; ");
-      for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i].split("=");
-        if (cookie[0] === name) {
-          return decodeURIComponent(cookie[1]);
-        }
-      }
-      return null;
-    }
-
-    let userEmail = getCookie("user_email");
     console.log("userEmail: ", userEmail);
 
     if (!userEmail) {
-      alert("Devi essere loggato per mettere Mi Piace!");
+      alert("Devi essere loggato per mettere like!");
       return;
     }
 
@@ -245,79 +226,62 @@ document.addEventListener("click", function (event) {
       console.error("articleId non trovato");
       return;
     }
-    const likeCountElement = document.getElementById(`like-article${articleId}`);
 
+    const likeCountElement = document.getElementById(`like-article${articleId}`);
     if (!likeCountElement) {
       console.error(`Elemento con ID like-article${articleId} non trovato`);
       return;
     }
-
-    let likedArticles = JSON.parse(localStorage.getItem("likedArticles")) || {};
-    console.log("liked articles: ", likedArticles);
-
-    if (likedArticles[articleId] && likedArticles[articleId][userEmail]) {
-      alert("Hai già messo Mi Piace a questo articolo!");
-      return;
-    }
-
-    
-    likedArticles[articleId] = likedArticles[articleId] || {};
-    likedArticles[articleId][userEmail] = true;
-    localStorage.setItem("likedArticles", JSON.stringify(likedArticles));
-
-    let currentLikes = parseInt(likeCountElement.textContent);
-    currentLikes++;
-    likeCountElement.textContent = currentLikes;
-
     
     fetch("http://127.0.0.1/Projects/like.php", {
       method: "POST",
       body: JSON.stringify({ articleId: articleId }),
-      headers: { 
-        "Content-Type": "application/json"
-      },
+      headers: { "Content-Type": "application/json" },
       credentials: "include"
     })
-    .then(response => response.text())
-    .then(data => {
-      console.log("Risposta dal server:", data);
-      return JSON.parse(data);
-    })
-    .then(json => {
-      if (json.success) {
-        console.log("Like aggiornato:", json);
+    .then(response => response.json())
+    .then(likeCountObject => {
+      if (likeCountObject.success) {
+        
+        likeCountElement.textContent = likeCountObject.likeCount;
+        console.log("Like aggiornato:", likeCountObject);
       } else {
-        console.error("Errore aggiornamento like:", json.error);
+        alert(likeCountObject.error);
+        console.error("Errore aggiornamento like:", likeCountObject.error);
       }
     })
     .catch(error => console.error("Errore nella richiesta:", error));
   }
-});
 
-// ---------------------------------------------------------------
-// Controllo sull'accesso alla sezione Book
-document.addEventListener("DOMContentLoaded", () => {
-  const booksLink = document.getElementById("Books");
-
-  // Aggiungi un event listener per controllare l'accesso alla pagina Books.html
-  booksLink.addEventListener("click", (event) => {
-    let userEmail = getCookie("user_email");
-
+  if (event.target && event.target.classList.contains("comment")) {
     if (!userEmail) {
-      // Se l'utente non è loggato, blocca l'accesso e mostra un avviso
-      alert("Devi prima fare il login per accedere alla sezione Books.");
-      event.preventDefault(); // Impedisce il comportamento di default (navigare a Books.html)
-    }
-  });
-});
-
-function getCookie(name) {
-  let cookies = document.cookie.split("; ");
-  for (let i = 0; i < cookies.length; i++) {
-    let cookie = cookies[i].split("=");
-    if (cookie[0] === name) {
-      return decodeURIComponent(cookie[1]);
+      alert("Devi essere autenticato per commentare");
+    } else {
+      const articleId = event.target.getAttribute("data-article");
+      window.location.href = `../commentPage.php?articleId=${articleId}`;
     }
   }
-  return null;
+});
+
+
+function getLikeCount(articleId) {
+  fetch('http://127.0.0.1/Projects/get_likes.php', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ articleId: articleId })
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.success) {
+          const likeElement = document.getElementById(`like-article${articleId}`);
+          if (likeElement) {
+              likeElement.innerText = data.likeCount;
+          } else {
+              console.error(`Elemento like-article${articleId} non trovato nel DOM`);
+          }
+      }
+  })
+  .catch(error => console.error('Errore nel recupero dei like:', error));
 }
